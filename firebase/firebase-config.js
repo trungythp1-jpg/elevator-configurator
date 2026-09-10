b2aa4745d5fedd2c4c8f55a807f405a878400a32
trungythp1-jpg <trungythp1@gmail.com>
2026-09-10 14:25:50 +0700

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,6 +1,3 @@
-// Firebase client configuration for Elevator Configurator.
-// This browser config is intended for client-side Firebase SDK use.
-// Security must be enforced by Firebase Authentication + Firestore Rules.
 export const firebaseConfig = {
   apiKey: "AIzaSyATAShAE4dBaU5fPAE1l_5sTe7WaUPumDA",
   authDomain: "elevator-configurator-ac760.firebaseapp.com",
